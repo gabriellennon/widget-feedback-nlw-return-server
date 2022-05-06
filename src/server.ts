8a83431c3dee933906d2a96d29c116f03147d6d5
frontend <gabriellennon@wizsolucoes.com.br>
2022-05-06 11:02:00 -0300

    sent email

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "725500b3edf204",
+      pass: "2ca2ac9af099e6"
+    }
+});
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
             screenshot
         }
     })
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Gabriel Lennon <gabriellennon@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
 
     //Return when has create the feedback
     return res.status(201).json({ data: feedback });
