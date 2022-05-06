26cc34a7fa5b58e27d486f6244b57e725640093b
frontend <gabriellennon@wizsolucoes.com.br>
2022-05-06 14:52:32 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use(routes);
 
